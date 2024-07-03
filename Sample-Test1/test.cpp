9e1f61a2fba05a32d0694659784cd1361f4616eb
jh.ko <jeonghunko@gmail.com>
2024-07-03 14:52:08 +0900

    [feature] p30

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -9,3 +9,10 @@ TEST(BaseballTest, Exception1) {
 	Baseball game;
 	EXPECT_THROW(game.guess(string("12")), length_error);
 }
+
+TEST(BaseballTest, Exception2) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+}
+
+
